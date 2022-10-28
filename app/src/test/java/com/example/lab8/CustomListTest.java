34f68df6d51a325816d0ea6c2271109f4492a931
Hishamanik <anikalhisham@gmail.com>
2022-10-28 02:35:06 -0600

    run1

--- app/src/test/java/com/example/lab8/CustomListTest.java
+++ app/src/test/java/com/example/lab8/CustomListTest.java
@@ -2,7 +2,7 @@ package com.example.lab8;
 
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.Assert.assertTrue;
+
 
 //import org.junit.Before;
 import org.junit.jupiter.api.BeforeAll;
@@ -11,51 +11,8 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 
 public class CustomListTest {
-    private CustomList list;
-    /**
-     * create a mocklist for my citylist
-     * @return
-     */
-    public CustomList MockCityList(){
-        list = new CustomList(null,new ArrayList<>());
-        return list;
-    }
 
-    /**
-     * get the size of the list
-     * increase the list by adding a new city
-     * check if our current size matches the initial size plus
-     one
-     */
-    @Test
-    public void addCityTest(){
-        list = MockCityList();
-        int listSize = list.getCount();
-        list.addCity(new City("Estevan", "SK"));
-        assertEquals(list.getCount(),listSize + 1);
-    }
+//    public void addCityTest(){};
+    addCity();
 
-    @Test
-    public void hasCityTest(){
-        list = MockCityList();
-        City city = new City("Estevan", "SK");
-        list.addCity(city);
-        assertTrue(list.hasCity(city));
-    }
-
-    @Test
-    public void deleteCityTest(){
-        list = MockCityList();
-        City city = new City("Estevan", "SK");
-        list.addCity(city);
-        list.deleteCity(city);
-        assertEquals(list.getCount(),0);
-    }
-
-    @Test
-    public void countCityTest(){
-        list = MockCityList();
-        list.addCity(new City("Estevan", "SK"));
-        assertEquals(list.countCity(),1);
-    }
 }
